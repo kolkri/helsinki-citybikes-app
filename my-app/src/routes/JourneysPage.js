abb51afed4a92828ed3c53ed62582480826d26e0
kolkri <kristiinakolu@gmail.com>
2022-08-10 22:44:32 +0300

    finish pagination

--- my-app/src/routes/JourneysPage.js
+++ my-app/src/routes/JourneysPage.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { API_URL } from '../utils/urls'
-import { Flex } from '@aws-amplify/ui-react';
-import styled from 'styled-components';
+import { Flex, Pagination } from '@aws-amplify/ui-react'
+import styled from 'styled-components'
+import _ from 'lodash'
 
 const pageSize = 10;
 const JourneysPage = () => {
 
 	const [journeys, setJourneys] = useState([])
 	const [loading, setLoading] = useState(false)
+	const [paginatedJourneys, setPaginatedJourneys] = useState()
+	const [currentpage, setCurrentpage] = useState(1)
 
 	useEffect( () =>{
 		fetchJourneys()
@@ -20,6 +23,7 @@ const JourneysPage = () => {
 		.then(data=>{
 			console.log('data:', data)
 			setJourneys(data.response)
+			setPaginatedJourneys(_(data.response).slice(0).take(pageSize).value())
 		})
 		.finally(()=>setLoading(false))
   	} 
@@ -33,14 +37,30 @@ const JourneysPage = () => {
 	  padding: 0.5rem;
 	`
 	const StyledTable = styled.table`
-	
+	  font-size: 12px;
+	  @media (min-width:786px) and (max-width:1024px) {
+		  font-size: 16px;
+	  }
+	  
+	  @media (min-width: 1025px) {
+		  font-size: 24px;
+	  }
 	`
 
 	const pageCount = journeys? Math.ceil(journeys.length / pageSize) : 0
+	if (pageCount === 1 ) return null
+	const pages = _.range(1, pageCount+1)
+
+	const pagination = (pageNo) => {
+		setCurrentpage(pageNo)
+		const startIndex = (pageNo - 1) * pageSize
+		const paginatedJourney = _(journeys).slice(startIndex).take(pageSize).value()
+		setPaginatedJourneys(paginatedJourney)
+	}
 	
 	return (
 		<StyledFlex>
-			<h1>Journeys | May 2021</h1>
+			<h3>Journeys | May 2021</h3>
 			<StyledTable>
 				<thead>
 					<tr>
@@ -52,7 +72,7 @@ const JourneysPage = () => {
 				</thead>
 				<tbody>
 					{journeys.length !== 0 &&
-					journeys.map((journey, index) =>{
+					paginatedJourneys.map((journey, index) =>{
 						return (
 							<tr key={index}>
 								<td>{journey.departureStationName}</td>
@@ -67,9 +87,17 @@ const JourneysPage = () => {
 			</StyledTable>
 			<nav className='d-flex justify-content-center'>
 				<ul className='pagination'>
-					<li className='page-link'>1</li>
-					<li className='page-link'>2</li>
-					<li className='page-link'>3</li>
+					{journeys.length !== 0 && 
+					pages.map(page => {
+						return (
+						<li className={
+							page === currentpage ? "page-item active" : "page-item"
+						}>
+							<p className="page-link"
+							onClick={() => pagination(page)}>{page}</p>
+						</li>
+						)}
+					)}
 				</ul>
 			</nav>
 		</StyledFlex>
